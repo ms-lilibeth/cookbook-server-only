2aa72950163a46c27dd4f4a96582984a1c666762
ms-lilibeth <ms.lilibeth@yandex.ru>
2016-10-24 01:41:18 +0500

    Trying to listen 8080 port

--- server.py
+++ server.py
@@ -110,5 +110,5 @@ def make_app():
 if __name__ == '__main__':
     print("Tornado works")
     app = make_app()
-    app.listen(443)
+    app.listen(8080)
     tornado.ioloop.IOLoop.current().start()
